1b79c02a75cfae7c4c56fb6d813b2b173e8de1a5
Cody Whitaker <codywhitaker@mwc-019240.dhcp.missouri.edu>
2018-06-22 11:05:27 -0500

    Documents Final Solution

---
+++ Documents/Documents/DocumentTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DocumentTableViewCell.swift
+//  Documents
+//
+//  Created by Cody Whitaker on 6/22/18.
+//  Copyright © 2018 Cody Whitaker. All rights reserved.
+//
+
+import UIKit
+
+class DocumentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
